9e2d115fb8ded1429f2eccbbd43464a303e60199
Fai549 <684259015@webmail.npru.ac.th>
2025-09-04 03:22:28 +0700

    Buy product3

---
+++ BuyProduct3.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+    public class BuyProduct3 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.print("Product #1 name: ");
+            String productName1 = scanner.nextLine();
+            System.out.print("product #1 price: ");
+            double productPrice1 = scanner.nextDouble();
+            System.out.print("Enter quantity for produuct 1 = ");
+            double quantity_product1 = scanner.nextDouble();
+            scanner.nextLine();
+
+            System.out.print("product #2 name: ");
+            String productName2 = scanner.nextLine(); 
+            System.out.print("Product #2 price: ");
+            double productPrice2 = scanner.nextDouble();
+            System.out.print("Enter quantity for produuct 2 = ");
+            double quantity_product2 = scanner.nextDouble();
+            scanner.nextLine();
+
+            System.out.print("Product #3 name: ");
+            String productName3 = scanner.nextLine();
+            System.out.print("Product #3 price: ");
+            double productPrice3 = scanner.nextDouble();
+            System.out.print("Enter quantity for produuct 3 = ");
+            double quantity_product3 = scanner.nextDouble();
+            scanner.nextLine();
+            
+            System.out.print("Enter amount paid : ");
+            double paid = scanner.nextDouble();
+            
+            System.out.println("----- receipt -----");
+
+            double totalProduct1 = productPrice1 * quantity_product1 ;
+            double totalProduct2 = productPrice2 * quantity_product2 ;
+            double totalProduct3 = productPrice3 * quantity_product3 ;
+            System.out.println(productName1 +" : " +productPrice1 +" x " +quantity_product1 +" = " +totalProduct1 +" baht.");
+            System.out.println(productName2 +" : " +productPrice2 +" x " +quantity_product2 +" = " +totalProduct2 +" baht.");
+            System.out.println(productName3 +" : " +productPrice3 +" x " +quantity_product3 +" = " +totalProduct3 +" baht.");
+            
+            double subtotal = totalProduct1 + totalProduct2 + totalProduct3 ;
+            System.out.println("Subtotal : " +subtotal );
+
+            double vat = subtotal * 0.07;
+            System.out.println("VAT = " +vat +" baht.");
+
+            double totalIncludingVat =  subtotal + vat ;
+            System.out.println("Total including VAT : " +totalIncludingVat +" Baht.");
+            System.out.println("Amount paid : ");
+            double change = subtotal - vat ;
+            System.out.println("Change: " +change +" Baht. "); 
+        }
+    }
